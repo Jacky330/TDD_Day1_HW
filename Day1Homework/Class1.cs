83928b446d578819b5250e3f0792bb05c78d304b
Jacky <jing330@gmail.com>
2016-07-04 22:17:58 +0800

    v1

---
+++ Day1Homework/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day1Homework
+{
+
+    public static class Grouping
+    {
+        public static decimal[] Subtotal<T>(this IEnumerable<T> data, int size, string colName)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentException("The argument 'size' must be greater than zero.");
+            }
+            if (string.IsNullOrWhiteSpace(colName) || colName.Contains(" "))
+            {
+                throw new ArgumentException("The argument 'colName' cannot be null, empty or contain any white space.");
+            }
+
+            int pageCount = (int)Math.Ceiling((double)data.Count() / size);
+            decimal[] result = new decimal[pageCount];
+
+            for (var i = 0; i < pageCount; ++i)
+            {
+                result[i] = data.Skip(i * size).Take(size).Select(d => Convert.ToDecimal(d.GetType().GetProperty(colName).GetValue(d, null))).Sum();
+            }
+            return result;
+        }
+    }
+}
---
+++ Day1HomeworkTests/GroupingTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day1Homework;
+using System.Collections.Generic;
+using ExpectedObjects;
+using FluentAssertions;
+using System;
+
+namespace Day1Homework.Tests
+{
+    [TestClass()]
+    public class GroupingTests
+    {
+
+        private IList<Product> GetProducts() {
+            return new List<Product>() {
+                new Product {Id=1,Cost=1,Revenue=11,SellPrice=21 },
+                new Product {Id=2,Cost=2,Revenue=12,SellPrice=22 },
+                new Product {Id=3,Cost=3,Revenue=13,SellPrice=23 },
+                new Product {Id=4,Cost=4,Revenue=14,SellPrice=24 },
+                new Product {Id=5,Cost=5,Revenue=15,SellPrice=25 },
+                new Product {Id=6,Cost=6,Revenue=16,SellPrice=26 },
+                new Product {Id=7,Cost=7,Revenue=17,SellPrice=27 },
+                new Product {Id=8,Cost=8,Revenue=18,SellPrice=28 },
+                new Product {Id=9,Cost=9,Revenue=19,SellPrice=29 },
+                new Product {Id=10,Cost=10,Revenue=20,SellPrice=30 },
+                new Product {Id=11,Cost=11,Revenue=21,SellPrice=31 }
+            };
+        }
+        private class Product
+        {
+            public int Id { get; set; }
+            public int Cost { get; set; }
+            public int Revenue { get; set; }
+            public int SellPrice { get; set; }
+        }
+
+        #region TestMethods
+        [TestMethod()]
+        public void TestForSubtotal_3筆一組_取Cost總和_結果應為6_15_24_21()
+        {
+            // arrange
+            var products = GetProducts();
+            var size = 3;
+            var colName = "Cost";
+
+            // atc
+            var actual = products.Subtotal(size, colName);
+
+            // assert
+            var expected = new decimal[] { 6, 15, 24, 21 };
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod()]
+        public void TestForSubtotal_4筆一組_取Revenue總和_結果應為50_66_60()
+        {
+            // arrange
+            var products = GetProducts();
+            var size = 4;
+            var colName = "Revenue";
+
+            // atc
+            var actual = products.Subtotal(size, colName);
+
+            // assert
+            var expected = new decimal[] { 50, 66, 60 };
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+        [TestMethod()]
+        public void TestForSubtotal_分組數量設為0_結果應回傳ArgumentException() {
+            // arrange
+            var products = GetProducts();
+            var size = 0;
+            var colName = "Revenue";
+
+            // act
+            Action act = () => products.Subtotal(size, colName);
+
+            // assert
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod()]
+        public void TestForSubtotal_分組數量設為1_屬性名稱設為Empty_結果應回傳ArgumentException()
+        {
+            // arrange
+            var products = GetProducts();
+            var size = 1;
+            var colName = string.Empty;
+
+            // act
+            Action act = () => products.Subtotal(size, colName);
+
+            // assert
+            act.ShouldThrow<ArgumentException>();
+        }
+        #endregion
+    }   
+}
